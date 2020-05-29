3fa445b08d6ab09ec0fbc0d451c25501f9f33d7b
contact9853 <contact9853@gmail.com>
2020-05-29 14:02:59 +0900

    계수 산정

--- proc_data.py
+++ proc_data.py
@@ -8,23 +8,23 @@ URL = 'data/export_abc_server_0527.csv'
 config = { 'trading_min': 100,
            'start' : '2020-04-01',
            'end' : '2020-05-31',
-           'incentive' : 140/61,
+           'incentive' : 70/31,
            'username' : 'bos992'
           }
 
 botnames = {
             'ua-coinsbit' : {'name': ['UA-COINSBIT', 'UA-COINSBIT2']},
             'keyt-flata': {'name': ['KEYT-FLATA'], },
-            # 'dpd-flata': {'name': ['DPD - FLATA', 'DPD-FLATA']},
-            # 'loa-btc-bithumb': {'name': ['BITHUMB - LOA - BTC', 'LOA-BTC-BITHUMBPRO'],},
-            # 'loa-usdt-bithumb': {'name': ['BITHUMB - LOA - USDT', 'LOA-BITHUMBPRO'], 'start': '2020-04-20', 'end': '2020-05-30'},
-            'zg-brx': {'name': ['ZG - BRX'], 'start': '2020-05-17', 'end': '2020-05-30'},
-            'nzc-flata': {'name': ['FLATA-NZC-2', 'FLATA - NZC', 'NZC-FLATA'], 'start': '2020-05-14', 'end': '2020-05-30'},
-            'brx-wbf' : {'name' : ['WBF - BRX'], 'start': '2020-05-07', 'end': '2020-05-30'},
-            'mtt-flata': {'name': ['FLATA - MTT', 'MTT-FLATA', 'flata - mtt'], 'start': '2020-05-06', 'end': '2020-05-30'},
-            'blc-flata': {'name': ['FLATA - BLC', 'BLC-FLATA'], 'start': '2020-05-06', 'end': '2020-05-30'},
-            'bl-flata': {'name': ['FLATA - BL', 'BL-FLATA'], 'start': '2020-05-06', 'end': '2020-05-30'},
-            'hug-exx' : {'name' : ['EXX - HUG'], 'start': '2020-05-06', 'end': '2020-05-30'},
+            'dpd-flata': {'name': ['DPD - FLATA', 'DPD-FLATA']},
+            'loa-btc-bithumb': {'name': ['BITHUMB - LOA - BTC', 'LOA-BTC-BITHUMBPRO'],},
+            'loa-usdt-bithumb': {'name': ['BITHUMB - LOA - USDT', 'LOA-BITHUMBPRO'], 'start': '2020-04-20', 'end': '2020-05-30'},
+            'zg-brx': {'name': ['ZG - BRX'], 'start': '2020-05-17', 'end': '2020-05-31'},
+            'nzc-flata': {'name': ['FLATA-NZC-2', 'FLATA - NZC', 'NZC-FLATA'], 'start': '2020-05-14', 'end': '2020-05-31'},
+            'brx-wbf' : {'name' : ['WBF - BRX'], 'start': '2020-05-07', 'end': '2020-05-31'},
+            'mtt-flata': {'name': ['FLATA - MTT', 'MTT-FLATA', 'flata - mtt'], 'start': '2020-05-06', 'end': '2020-05-31'},
+            'blc-flata': {'name': ['FLATA - BLC', 'BLC-FLATA'], 'start': '2020-05-06', 'end': '2020-05-31'},
+            'bl-flata': {'name': ['FLATA - BL', 'BL-FLATA'], 'start': '2020-05-06', 'end': '2020-05-31'},
+            'hug-exx' : {'name' : ['EXX - HUG'], 'start': '2020-05-06', 'end': '2020-05-31'},
            }
 
 
@@ -60,7 +60,7 @@ def report(bots):
             bot['date'] = result
             start_date = bot['date'][0][0] if bot['date'] else None
             end_date   = bot['date'][-1][0] if bot['date'] else None
-            print('%d. %s' % (i, bot['botname']))
+            print('%d. %s' % (i+1, bot['botname']))
             print('date range : {} ~ {}' .format(start_date, end_date))
 
             writer.writerow(['%d. %s' % (i, bot['botname'])])
@@ -79,7 +79,7 @@ def report(bots):
             print('total validity date : %d/%d'% (cnt, tot_cnt))
             print('incentive {:.4f} won' .format(incentive))
             writer.writerow(['total operation date : %s' % ','.join(result)])
-            writer.writerow(['total validity date : %d'% cnt])
+            writer.writerow(['total validity date : %d/%d'% (cnt, tot_cnt)])
             writer.writerow(['incentive {:.4f} won' .format(incentive)])
 
         print('==================================================')
