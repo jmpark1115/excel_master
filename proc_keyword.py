3731f022a311e9bd810fc865dd2fb617a1ebe470
contact9853 <contact9853@gmail.com>
2020-11-03 15:47:37 +0900

    추가

--- proc_keyword.py
+++ proc_keyword.py
@@ -5,14 +5,14 @@ from os import listdir
 from os.path import isfile, join
 import sys
 
-filepath = 'logs1103_2'
+filepath = 'logs1103_2'   # 대표의 리포트 요청
+# filepath = 'logs1103_3'  #  해커공격 신고
+
 files = [f for f in listdir(filepath) if isfile(join(filepath, f))]
 files = [x for x in files if x.find("logfile") != -1]
 
-# file_r = 'logs1103/logfile.txt'
-
-file_w = 'logs1103_2/result_{}.txt'.format(datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
-keyword = '!!! Found Hacker'
+file_w = filepath + '/result_{}.txt'.format(datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
+keyword = '!!!'
 
 with open(file_w, 'a', encoding='utf-8') as fw:
     tot = 0
